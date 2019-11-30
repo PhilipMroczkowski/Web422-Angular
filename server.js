18db45516b7091fdb770c4b0487830716f2ae6f2
Your NamePhilip <philip.mroczkowski@yahoo.com>
2019-11-30 15:39:50 -0500

    ok

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
-// setup a 'route' to listen on the default url path
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+var HTTP_PORT = process.env.PORT || 8080;
 
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+// setup the static folder 
+app.use(express.static("public")); 
+
+// Start the server
+
+app.listen(HTTP_PORT, function(){
+    console.log("Server listening on port: " + HTTP_PORT);
+});
